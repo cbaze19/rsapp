9dc1a0bd3c597895cabe1fc94c3e9eb3814031fd
cbaze19 <cbaze19@gmail.com>
2015-04-11 17:17:19 -0500

    added WoT Functionality

--- server.rb
+++ server.rb
@@ -3,7 +3,18 @@ require 'thin'
 require 'mechanize'
 
 get '/' do
+	redirect '/rs'
+end
 
+get '/rs' do
+	rsCalc()
+end
+
+get '/wot' do
+	wotCalc()
+end
+
+def rsCalc()
 	agent = Mechanize.new
 
 	page = agent.get "http://services.runescape.com/m=hiscore_oldschool/overall.ws"
@@ -28,5 +39,36 @@ get '/' do
 		exps.push exp
 	end
 
-	erb :index, :locals => {:skillNames => names, :skillLevels => levels, :skillExps => exps}
+	erb :rs, :locals => {:skillNames => names, :skillLevels => levels, :skillExps => exps}
+end
+
+def wotCalc()
+	agent = Mechanize.new
+
+	page = agent.get "http://worldoftanks.com/community/accounts/1001630827-c_dizzle/"
+
+	names = Array.new
+	values = Array.new
+
+	names.push 'Personal Rating'
+	names.push 'Win/Loss'
+	names.push 'Battles Fought'
+	names.push 'Experience Per Battle'
+	names.push 'Damage Per Battle'
+	names.push 'Kill/Death'
+	names.push 'Damage Caused/Received'
+	names.push 'Achievements'
+
+	values.push page.search('p.t-personal-data_value__pr')[0].text
+
+	for i in 0..3
+		val = page.search('td.t-personal-data_value')[i].text.strip
+		values.push val
+	end
+
+	values.push page.search('p.b-speedometer-weight')[0].text
+	values.push page.search('p.b-speedometer-weight')[1].text
+	values.push page.search('h3.js-achievements-header')[0].text.slice(13,100)
+
+	erb :wot, :locals => {:names => names, :values => values}
 end
